406a03b61e23c6ae7189464a1b852ffcf87999a4
Mark Faust <markfaust123@gmail.com>
2024-09-30 08:57:00 -0400

    created final test to create new user then get currently logged in user.

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,15 +16,33 @@ describe('Authentication System (e2e)', () => {
   });
 
   it('handles a signup request', () => {
-    const test_email = 'asdfjkl2@gmail.com'
+    const test_email = 'asdfjkl2@gmail.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email: test_email, password: 'asdfjkl'})
+      .send({ email: test_email, password: 'asdfjkl' })
       .expect(201)
       .then((response) => {
-        const {id, email } = response.body
+        const { id, email } = response.body;
         expect(id).toBeDefined();
-        expect(email).toEqual(test_email)
+        expect(email).toEqual(test_email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const test_email = 'asdf@gmail.com';
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: test_email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+      expect(body.email).toEqual(test_email);
   });
 });
