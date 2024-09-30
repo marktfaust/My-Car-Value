4a9de3c33c45e46922f900615a1b36021143fedd
Mark Faust <markfaust123@gmail.com>
2024-09-30 08:49:09 -0400

    added comment to fix git message of purpose of setup.ts. Purpose is to really make sure that all test databases are DELETED before each successive test execution, not created as the previous git message stated.

--- test/setup.ts
+++ test/setup.ts
@@ -2,7 +2,11 @@ import { rm } from 'fs/promises';
 import { join } from 'path';
 
 global.beforeEach(async () => {
+  /* Deletes test databases that are created during test runs before execution of next test. 
+  This way, can run "signup" or similar test without having to change hardcoded test email each time! */ 
   try {
     await rm(join(__dirname, '..', 'test.sqlite'));
-  } catch (err) {}
+  } catch (err) {
+    /* Don't throw error if test.sqlite file does not already exist */
+  }
 });
