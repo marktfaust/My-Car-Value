72e522ba991707fd0580ee17d2d7ff744f32766a
Mark Faust <markfaust123@gmail.com>
2024-10-20 09:53:56 -0400

    created route handler for createReport. protecting with AuthGuard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
+    constructor(private reportsService: ReportsService) {}
 
+    @UseGuards(AuthGuard)
     @Post()
     createReport(@Body() body: CreateReportDto) {
-
+        return this.reportsService.create(body);
     }
 }
