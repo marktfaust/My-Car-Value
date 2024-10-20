e183eb12f617a68e253bbd92a7e1c9ceca833b41
Mark Faust <markfaust123@gmail.com>
2024-10-20 09:51:25 -0400

    create creat report dto to define structure of data coming into applicaion

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateReportDto } from './create-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+    @Post()
+    createReport(@Body() body: CreateReportDto) {
+
+    }
+}
